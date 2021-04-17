3d1c7b3abf36e25e5cff63cc920f9de7d5c6785e
ramin-sh <33373883+ramin-sh@users.noreply.github.com>
2021-04-17 15:06:12 +0000

    Add files via upload

---
+++ list/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('list/', views.to_do, name='to_do'),
+    path('delete_todo/<int:todo_id>/', views.delete_todo, name='delete_todo'),
+    path('a',views.show_temp,name='show_temp')
+]
---
+++ list/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.utils import timezone
+from .models import Todo
+from django.http import HttpResponseRedirect
+from django.contrib.auth.models import User
+
+# Create your views here.
+def index(request):
+    todo_items = Todo.objects.all().order_by()
+    #print(todo_items)   --> for see todo_items value in cmd
+    
+
+    return render(request, "list/index.html" , {"todo_items": todo_items})
+    
+def to_do(request):
+    current_date = timezone.now()
+    content = request.POST.get('content')
+    Todo.objects.create(added_date=current_date, text=content)
+    return HttpResponseRedirect("/")
+
+def delete_todo(request, todo_id):
+    Todo.objects.get(id=todo_id).delete()
+    return HttpResponseRedirect("/")
+def show_temp(request):
+    return render(request,"list/a.html")
