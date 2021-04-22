bb25523189dba681ea940c3f686c8430d35151ac
ramin-sh <33373883+ramin-sh@users.noreply.github.com>
2021-04-22 20:08:25 +0430

    Update views.py

--- list/views.py
+++ list/views.py
@@ -7,9 +7,6 @@ from django.contrib.auth.models import User
 # Create your views here.
 def index(request):
     todo_items = Todo.objects.all().order_by()
-    #print(todo_items)   --> for see todo_items value in cmd
-    
-
     return render(request, "list/index.html" , {"todo_items": todo_items})
     
 def to_do(request):
@@ -21,5 +18,3 @@ def to_do(request):
 def delete_todo(request, todo_id):
     Todo.objects.get(id=todo_id).delete()
     return HttpResponseRedirect("/")
-def show_temp(request):
-    return render(request,"list/a.html")
