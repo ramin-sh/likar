88a379e59c256445d7e21eb58bd3b80dafb707ba
ramin-sh <33373883+ramin-sh@users.noreply.github.com>
2021-04-22 20:06:23 +0430

    Update urls.py

--- list/urls.py
+++ list/urls.py
@@ -3,6 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.to_do, name='to_do'),
-    path('delete_todo/<int:todo_id>/', views.delete_todo, name='delete_todo'),
-    path('a',views.show_temp,name='show_temp')
+    path('delete_todo/<int:todo_id>/', views.delete_todo, name='delete_todo'), 
 ]
